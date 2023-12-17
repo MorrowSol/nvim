2d4232dd66b9738b4eae79ebedec59dcc2fbebe6
chen <179330144@qq.com>
2023-12-17 15:38:27 +0800

    add trouble indent-blankline toggleterm lazygit

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,19 @@
+return {
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	config = function()
+		require("toggleterm").setup({
+			open_mapping = "<C-/>",
+			direction = "float",
+			shade_terminals = true,
+		})
+		local Terminal = require("toggleterm.terminal").Terminal
+		local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+
+		function _lazygit_toggle()
+			lazygit:toggle()
+		end
+
+		vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+	end,
+}
